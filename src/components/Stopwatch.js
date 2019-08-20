e54010c482a4c7d21ff57f069dcbe886db2baf9e
wrotich <winniecherop@outlook.com>
2019-08-20 16:07:00 +0300

    add state for the stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
-
 class Stopwatch extends Component {
+
+  state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => this.tick(), 100)
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState( prevState => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+      }));
+    }
+  }
+
+  handleStopWatch = () => {
+    this.setState( prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={this.handleStopWatch}>
+          { this.state.isRunning ? 'Stop' : 'Start' }</button>
         <button>Reset</button>
       </div>
     );
