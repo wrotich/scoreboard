eb1d01c0d5f9def4b82261e864d07d0e405936e3
wrotich <winniecherop@outlook.com>
2019-08-21 18:05:58 +0300

    add crown image to the player with the highest score

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Counter from './Counter';
+import Icon from './Icon';
+
 
 class Player extends PureComponent {
 
@@ -10,7 +12,8 @@ class Player extends PureComponent {
     name: PropTypes.string.isRequired,
     score: PropTypes.number.isRequired,
     id: PropTypes.number,
-    index: PropTypes.number
+    index: PropTypes.number,
+    isHighScore: PropTypes.bool
   };
 
   render() {
@@ -26,6 +29,7 @@ class Player extends PureComponent {
       <div className="player">
         <span className="player-name">
           <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+          <Icon isHighScore={this.props.isHighScore}/>
           { name }
         </span>
 
