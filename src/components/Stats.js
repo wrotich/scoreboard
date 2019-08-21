58bf35188bceba7298d7f3f3f39cd9c33bcfe524
wrotich <winniecherop@outlook.com>
2019-08-21 17:39:32 +0300

    validate props using PropTypes

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Stats = ({ players }) => {
 
@@ -22,4 +23,11 @@ const Stats = ({ players }) => {
     </table>
   );
 }
+
+Stats.propTypes = {
+  players: PropTypes.arrayOf(PropTypes.shape({
+    score: PropTypes.number
+  }))
+};
+
 export default Stats;
